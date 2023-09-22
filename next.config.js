d0deeb4869ae8ccb3b0f17b81c29b8779cb3874f
Rajvir Singh <rsaggu99@gmail.com>
2023-09-22 16:19:16 +0530

    chore: Added first page of the portfolio refresh

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  // Added SCSS support in the app
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  }
 }
 
 module.exports = nextConfig
