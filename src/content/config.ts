93c95e228589ac097d0ecc5e32057465a18336e3
Rajvir Singh <rsaggu99@gmail.com>
2024-12-30 18:18:33 +0530

    fix: remover author from posts collection defintion

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,6 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
     schema: z.object({
         title: z.string(),
-        author: z.string(),
         date: z.date(),
         image_url: z.string(),
     })
