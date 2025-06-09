5f2f3db715f5a9aa62b8629495ab497809b8a032
Rajvir Singh <rsaggu99@gmail.com>
2025-06-10 00:17:34 +0530

    chore: added projects and contribtions

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import BlogPost from "./BlogPost.astro";
+import Footer from "./Footer.astro";
+import Navbar from "./Navbar.astro";
+import ProjectCard from "./ProjectCard.astro";
+import ThemeSwitcher from "./ThemeSwitcher.astro";
+
+export { BlogPost, Footer, Navbar, ProjectCard, ThemeSwitcher }
---
+++ src/icons/index.ts
@@ -0,0 +1,14 @@
+// dark light mode icons
+import DarkMode from "./DarkMode.astro";
+import LightMode from "./LightMode.astro";
+
+//social icons
+import Linkedin from "./Linkedin.astro";
+import Github from "./Github.astro";
+import X from "./X.astro";
+import Youtube from "./Youtube.astro";
+
+// other interaction icons
+import LongDownArrow from "./LongDownArrow.astro";
+
+export { DarkMode, LightMode, Linkedin, Github, X, Youtube, LongDownArrow }
