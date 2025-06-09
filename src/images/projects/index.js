ae8d8aa8f5694b43ff712189e34cd4da05764a83
Rajvir Singh <rsaggu99@gmail.com>
2025-06-10 00:17:34 +0530

    chore: added projects and contribtions

---
+++ src/images/projects/index.js
@@ -0,0 +1,6 @@
+import bondium from './bondium.png';
+import benvenuto from './benvenuto.png';
+import mazzolari from './mazzolari.png';
+import acNautica from './ac_nautica.png';
+
+export { bondium, mazzolari, benvenuto, acNautica };
