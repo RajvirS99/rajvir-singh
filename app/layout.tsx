e226667cb35549e535de3383cde81be92f328b2f
Rajvir Singh <rajvir@apyhub.com>
2026-04-17 03:14:14 +0530

    Feat: added google analytics to next js portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,8 @@ export default function RootLayout({
           }}
         />
       </head>
+      {/* <GoogleTagManager gtmId="GTM-XYZ" /> */}
+      <GoogleAnalytics gaId="G-ZX1VRSZ3MN" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
